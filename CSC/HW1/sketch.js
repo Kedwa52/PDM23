1781f74e470a2c41653f0863e46f368578604118
Kedwa52 <kedwa52@lsu.edu>
2023-01-24 14:00:33 -0600

    drew a smile

--- CSC/HW1/sketch.js
+++ CSC/HW1/sketch.js
@@ -1,7 +1,21 @@
+let x= 200;
+let y= 200;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(0);
+  background(220);
+
+  x = mousex;
+  y = mousey;
+
+  //draw face
+  fill('white');
+  circle(x,7,58);
+  fill('black');
+  square(x,y-10,5);
+  square(x+10,y-10,5);
+  square(x+5,y+5,5);
 }
