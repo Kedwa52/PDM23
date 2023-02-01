5c8f34af362ecf459a1d83e6185e58666f243d24
Kedwa52 <123499072+Kedwa52@users.noreply.github.com>
2023-01-31 19:02:09 -0600

    add files from pic 1

--- CSC/HW1/sketch.js
+++ CSC/HW1/sketch.js
@@ -1,21 +1,19 @@
-let x= 200;
-let y= 200;
-
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(10);
-
-  x = mousex;
-  y = mousey;
-
-  //draw face
-  fill('white');
-  circle(x,7,58);
-  fill('black');
-  square(x,y-10,5);
-  square(x+10,y-10,5);
-  square(x+5,y+5,5);
+  background(255);
+  //red opaque
+  noStroke();
+  fill(255, 0, 0, 100);
+  ellipse(200, 175, 150);
+  //blue opaque
+  noStroke();
+  fill(0, 0, 255, 100);
+  ellipse(150, 250, 150);
+  //green opaque
+  noStroke();
+  fill(0, 255, 0, 100);
+  ellipse(250, 250, 150);
 }
