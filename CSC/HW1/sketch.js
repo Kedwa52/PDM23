c82da9cd3285374672b81ce2c7639624429dca39
Kedwa52 <123499072+Kedwa52@users.noreply.github.com>
2023-01-31 19:18:29 -0600

    PIC 2 upload

--- CSC/HW1/sketch.js
+++ CSC/HW1/sketch.js
@@ -1,19 +1,9 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
 }
 
 function draw() {
-  background(255);
-  //red opaque
-  noStroke();
-  fill(255, 0, 0, 100);
-  ellipse(200, 175, 150);
-  //blue opaque
-  noStroke();
-  fill(0, 0, 255, 100);
-  ellipse(150, 250, 150);
-  //green opaque
-  noStroke();
-  fill(0, 255, 0, 100);
-  ellipse(250, 250, 150);
+  background(100,250,74);
+  ellipse(200,200,325,325)
+  square(400,50,325)
 }
